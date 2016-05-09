b4cbb3052b8a177924e319263ffd5939f24cb77f
mkorman9 <dynax126@gmail.com>
2016-05-09 18:08:35 +0200

    Tests for get method

--- src/test/scala/com/github/mkorman9/DynamoIntegrationTest.scala
+++ src/test/scala/com/github/mkorman9/DynamoIntegrationTest.scala
@@ -23,11 +23,12 @@ class DynamoIntegrationTest extends FunSuite with Matchers with BeforeAndAfterAl
   }
 
   test("Mapper should persist correct set of data with hash and sort key") {
+    val catToDelete = CatDataModel("Leila", "Hunter", None, new DateTime().minusYears(12))
     val cats = List(CatDataModel("Johnny", "Hunter", Some(112), new DateTime().minusYears(7)),
       CatDataModel("Mike", "Worker", Some(41), new DateTime().minusYears(3)),
       CatDataModel("Pablo", "Hunter", Some(117), new DateTime().minusYears(1)),
       CatDataModel("Ricky", "Unemployed", None, new DateTime().minusYears(2)),
-      CatDataModel("Leila", "Hunter", None, new DateTime().minusYears(12))
+      catToDelete
     )
 
     CatsMapping.putAll(cats)
@@ -35,11 +36,17 @@ class DynamoIntegrationTest extends FunSuite with Matchers with BeforeAndAfterAl
     val huntersBeforeRemoving = CatsMapping query {
       "roleName" -> cond.eq("Hunter") :: Nil
     }
+    val catToDeleteFromDb = CatsMapping.get("Hunter", "Leila")
+
     CatsMapping.delete("Hunter", "Leila")
+
     val huntersAfterRemoving = CatsMapping query {
       "roleName" -> cond.eq("Hunter") :: Nil
     }
+    val deletedCat = CatsMapping.get("Hunter", "Leila")
 
+    catToDeleteFromDb should be (Some(catToDelete))
+    deletedCat should be (None)
     huntersBeforeRemoving.size should be(3)
     huntersAfterRemoving.size should be(2)
     huntersBeforeRemoving forall (cats.contains(_)) should be(true)
@@ -47,7 +54,8 @@ class DynamoIntegrationTest extends FunSuite with Matchers with BeforeAndAfterAl
   }
 
   test("Mapper should persist correct set of data with just hash key") {
-    val dogs = List(DogDataModel("Max", List("black", "white")),
+    val dogToDelete = DogDataModel("Max", List("black", "white"))
+    val dogs = List(dogToDelete,
       DogDataModel("Rex", List("brown", "white"))
     )
 
@@ -56,11 +64,17 @@ class DynamoIntegrationTest extends FunSuite with Matchers with BeforeAndAfterAl
     val maxBeforeRemoving = DogsMapping query {
       "name" -> cond.eq("Max") :: Nil
     }
+    val dogToDeleteFromDb = DogsMapping.get("Max")
+
     DogsMapping.delete("Max")
+
     val maxAfterRemoving = DogsMapping query {
       "name" -> cond.eq("Max") :: Nil
     }
+    val deletedDog = DogsMapping.get("Max")
 
+    DogsMapping should be (Some(dogToDelete))
+    deletedDog should be (None)
     maxBeforeRemoving.size should be(1)
     maxAfterRemoving.size should be(0)
     dogs.contains(maxBeforeRemoving.head) should be(true)
